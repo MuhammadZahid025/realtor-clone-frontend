b3efdea52217b04464300885e9aff667e12ff413
kwanso-zahid <muhammad.zahid@kwanso.com>
2022-11-29 00:10:14 +0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import {BrowserRouter as Router , Routes, Route} from "react-router-dom"
+import { Forgotpassword } from "./pages/Forgotpassword";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/log-in" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
+        <Route path="/profile" element={<Profile />} />
+
+
+      </Routes>
+    </Router>
     </>
   );
 }
